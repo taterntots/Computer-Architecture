9c4b0f37ac4940d33aeb2b85f94da3895840a4de
taterntots <matthew.a.weidner@gmail.com>
2020-05-18 18:18:47 -0400

    got the CPU running and printing 8! MVP for today complete

--- interview_prep.py
+++ interview_prep.py
@@ -2,15 +2,35 @@
 # -----------------Understanding------------------
 # ------------------------------------------------
 
-
+# Given a dictionary with both strings and numbers for either keys and values,
+# we want to print the sume of all values that are numbers
 
 # ------------------------------------------------
 # --------------------Planning--------------------
 # ------------------------------------------------
 
-
+# Loop through the dictionary, printing all values from their key pairs
+  # If the value is a number, add them together
 
 # ------------------------------------------------
 # ------------------Execution---------------------
 # ------------------------------------------------
 
+dict = {
+  "cat": "bob",
+  "dog": 23,
+  19: 18,
+  90: "fish"
+}
+
+def sumdict(dictionary):
+  newArray = []
+  for i in dictionary.values():
+    # print(i)
+    if type(i) == int:
+      newArray.append(i)
+      print(newArray)
+  return sum(newArray)
+
+
+print(sumdict(dict))
--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,10 @@
 
 import sys
 
+LDI = 0b10000010
+HLT = 0b00000001
+PRN = 0b01000111
+
 class CPU:
     """Main CPU class."""
 
@@ -10,6 +14,26 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
         self.ram = [0] * 256
+        self.running = True
+        # self.ops = {
+        #     'ADD': '10100000 00000aaa 00000bbb',
+        #     'SUB':  '10100001 00000aaa 00000bbb',
+        #     'MUL': '10100010 00000aaa 00000bbb',
+        #     'DIV':  '10100011 00000aaa 00000bbb',
+        #     'MOD':  '10100100 00000aaa 00000bbb',
+
+        #     'INC':  '01100101 00000rrr',
+        #     'DEC':  '01100110 00000rrr',
+
+        #     'CMP': '10100111 00000aaa 00000bbb',
+
+        #     'AND': '10101000 00000aaa 00000bbb',
+        #     'NOT': '01101001 00000rrr',
+        #     'OR': '10101010 00000aaa 00000bbb',
+        #     'XOR': '10101011 00000aaa 00000bbb',
+        #     'SHL': '10101100 00000aaa 00000bbb',
+        #     'SHR': '10101101 00000aaa 00000bbb'
+        # }
         # stack pointer will need an initial value
 
     def ram_read(self, MAR):
@@ -17,6 +41,10 @@ class CPU:
 
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
+
+    def op_hit(self, operand_a, operand_b):
+        self.running = False
+        sys.exit(1)
 
     def load(self):
         """Load a program into memory."""
@@ -72,3 +100,21 @@ class CPU:
     def run(self):
         """Run the CPU."""
         self.trace()
+
+        while self.running:
+            # IR is the instruction register
+            IR = self.ram_read(self.pc)
+
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
+
+            if IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            elif IR == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2 
+
+            elif IR == HLT:
+                self.running == False

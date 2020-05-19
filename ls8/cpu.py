b2173188633167059db3375c10c54630b36bd27e
taterntots <matthew.a.weidner@gmail.com>
2020-05-19 17:32:34 -0400

    beautified my run to be without a bunch of if else statements

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -16,28 +16,11 @@ class CPU:
         self.pc = 0
         self.ram = [0] * 256
         self.running = True
-        # self.ops = {
-            # LDI: 0b10000010,
-            # HLT: 0b00000001,
-            # PRN: 0b01000111,
-            # MUL: 0b10100010
-        #     'ADD': '10100000 00000aaa 00000bbb',
-        #     'SUB':  '10100001 00000aaa 00000bbb',
-        #     'DIV':  '10100011 00000aaa 00000bbb',
-        #     'MOD':  '10100100 00000aaa 00000bbb',
-
-        #     'INC':  '01100101 00000rrr',
-        #     'DEC':  '01100110 00000rrr',
-
-        #     'CMP': '10100111 00000aaa 00000bbb',
-
-        #     'AND': '10101000 00000aaa 00000bbb',
-        #     'NOT': '01101001 00000rrr',
-        #     'OR': '10101010 00000aaa 00000bbb',
-        #     'XOR': '10101011 00000aaa 00000bbb',
-        #     'SHL': '10101100 00000aaa 00000bbb',
-        #     'SHR': '10101101 00000aaa 00000bbb'
-        # }
+        self.branchtable = {}
+        self.branchtable[LDI] = self.handle_LDI
+        self.branchtable[HLT] = self.handle_HLT
+        self.branchtable[PRN] = self.handle_PRN
+        self.branchtable[MUL] = self.handle_MUL
         # stack pointer will need an initial value
 
     def ram_read(self, MAR):
@@ -46,16 +29,27 @@ class CPU:
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
-    # def op_hit(self, operand_a, operand_b):
-    #     self.running = False
-    #     sys.exit(1)
+    def handle_LDI(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        self.pc += 3
 
-    def load(self):
+    def handle_HLT(self, operand_a, operand_b):
+        self.running == False
+
+    def handle_PRN(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+        self.pc += 2 
+
+    def handle_MUL(self, operand_a, operand_b):
+        self.alu('MUL', operand_a, operand_b)
+        self.pc += 3
+
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
-        with open(sys.argv[1]) as file:
+        with open(filename) as file:
             for line in file:
                 string_val = line.split('#')[0].strip()
                 if string_val == '':
@@ -102,21 +96,12 @@ class CPU:
         while self.running:
             # IR is the instruction register
             IR = self.ram_read(self.pc)
+            # print(IR)
 
             operand_a = self.ram_read(self.pc+1)
             operand_b = self.ram_read(self.pc+2)
 
-            if IR == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-
-            elif IR == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2 
-            
-            elif IR == MUL:
-                self.alu('MUL', operand_a, operand_b)
-                self.pc += 3
-
-            elif IR == HLT:
-                self.running == False
+            if IR == HLT:
+                self.running = False
+            else:
+                self.branchtable[IR](operand_a, operand_b)
